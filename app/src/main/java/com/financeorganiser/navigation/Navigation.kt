ba472298f8cdfb9aac0c058f6209c952803e2fd0
olekslukian <olekslukan@gmail.com>
2024-10-24 15:20:31 +0200

    test commit

--- app/src/main/java/com/financeorganiser/navigation/Navigation.kt
+++ app/src/main/java/com/financeorganiser/navigation/Navigation.kt
@@ -8,7 +8,7 @@ import androidx.navigation.compose.rememberNavController
 fun Navigation() {
     val navController = rememberNavController()
     
-    NavHost(navController = navController, startDestination = ) {
+    NavHost(navController = navController, startDestination = "test") {
         
     }    
 }
