53e2051d9093bf46e6d3fdcf0d87710da365988a
olekslukian <olekslukan@gmail.com>
2024-10-24 17:05:07 +0200

    Fix navigation animation

--- app/src/main/java/com/financeorganiser/features/home/HomeView.kt
+++ app/src/main/java/com/financeorganiser/features/home/HomeView.kt
@@ -12,7 +12,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.financeorganiser.navigation.Screen
 import com.financeorganiser.ui.Constants
--- app/src/main/java/com/financeorganiser/navigation/Navigation.kt
+++ app/src/main/java/com/financeorganiser/navigation/Navigation.kt
@@ -1,5 +1,7 @@
 package com.financeorganiser.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -10,13 +12,53 @@ import com.financeorganiser.features.home.HomeView
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
-    
-    NavHost(navController = navController, startDestination = Screen.AuthScreen.route) {
-    composable(route = Screen.AuthScreen.route) {
-        AuthView(navController)
+
+    NavHost(navController = navController, startDestination = Screen.AuthScreen.route,
+        enterTransition = {
+            slideIntoContainer(
+                AnimatedContentTransitionScope.SlideDirection.Start,
+                tween(700)
+            )
+        },
+        exitTransition = {
+            slideOutOfContainer(
+                AnimatedContentTransitionScope.SlideDirection.End,
+                tween(700)
+            )
+        },
+        popEnterTransition = {
+            slideIntoContainer(
+                AnimatedContentTransitionScope.SlideDirection.Start,
+                tween(700)
+            )
+        },
+        popExitTransition = {
+            slideOutOfContainer(
+                AnimatedContentTransitionScope.SlideDirection.End,
+                tween(700)
+            )
+        }
+    ) {
+        composable(
+            route = Screen.AuthScreen.route,
+            enterTransition = {
+                slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.End,
+                    tween(700)
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Start,
+                    tween(700)
+                )
+            },
+
+        ) {
+            AuthView(navController)
+        }
+        composable(route = Screen.HomeScreen.route) {
+            HomeView(navController)
+        }
     }
-    composable(route = Screen.HomeScreen.route) {
-        HomeView(navController)
-    }
-    }    
 }
